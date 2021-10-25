bf91b67637f9ce830b0861e7ae86c04706ba9fcd
Lucas-Porras <abdhalalucas@gmail.com>
2021-10-25 19:28:28 -0300

    El navbar esta en el partials (Los que no estan ni registrados ni logueados)

---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+    require_once 'database/database.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a la pasteleria!</title>
+    <link rel="shortcut icon" href="favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <style>
+        .col-12{
+            background-color: red;
+        }
+        #footer{
+        }
+    </style>
+</head>
+<body>
+    <?php include 'partials/header.php' ?>
+    <div class = "container mt-5">
+        <div class = "row">
+            <div class = "col-12">
+                asd
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+<!-- Footer Links -->
+<div id = "footer" class="text-center text-md-left">
+
+  <!-- Grid row -->
+  <div class="container row">
+
+    <!-- Grid column -->
+    <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+      <!-- Content -->
+      <h5 class="font-weight-bold text-uppercase mb-4">Footer Content</h5>
+      <p>Here you can use rows and columns to organize your footer content.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
+        esse
+        quasi, veritatis totam voluptas nostrum.</p>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+      <!-- Links -->
+      <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+
+      <ul class="list-unstyled">
+        <li>
+          <p>
+            <a href="#!">PROJECTS</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="#!">ABOUT US</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="#!">BLOG</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="#!">AWARDS</a>
+          </p>
+        </li>
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+      <!-- Contact details -->
+      <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+      <ul class="list-unstyled">
+        <li>
+          <p>
+            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+        </li>
+        <li>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+        </li>
+        <li>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        </li>
+        <li>
+          <p>
+            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+        </li>
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+      <!-- Social buttons -->
+      <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+      <!-- Facebook -->
+      <a type="button" class="btn-floating btn-fb">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <!-- Twitter -->
+      <a type="button" class="btn-floating btn-tw">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <!-- Google +-->
+      <a type="button" class="btn-floating btn-gplus">
+        <i class="fab fa-google-plus-g"></i>
+      </a>
+      <!-- Dribbble -->
+      <a type="button" class="btn-floating btn-dribbble">
+        <i class="fab fa-dribbble"></i>
+      </a>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2020 Copyright:
+  <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</body>
+</html>
