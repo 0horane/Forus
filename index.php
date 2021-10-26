713c110d8fbd2de37fb4a815b7a8e92c4a6ec464
Lucas-Porras <abdhalalucas@gmail.com>
2021-10-25 23:51:36 -0300

    Agrego los archivos que hice hoy (Lucas)

--- index.php
+++ index.php
@@ -1,145 +1,152 @@
 <?php
     require_once 'database/database.php';
- require_once 'session_start.php'; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido a la pasteleria!</title>
-    <link rel="shortcut icon" href="favicon.png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <style>
-        .col-12{
-            background-color: red;
-        }
-        #footer{
-        }
-    </style>
-    
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bienvenido a la pasteleria!</title>
+  <link rel="shortcut icon" href="favicon.png">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
+<style>
+  body{
+    background-clip: url("vids/backgroundVid.mp4");
+  }
+  header{
+    width: 100%;
+    height: auto;
+    position: relative;
+    overflow: hidden;
+  }
+  video{
+    width: 100%;
+  }
+  #absoluto{
+    color: white;
+    width: 100%;
+    height: 100%;
+    top: 32%;
+    right: 0;
+    position: absolute;
+  }
+  footer{
+    border-top: 2.5px solid #f1f0f0;
+  }
+</style>
 <body>
-    <?php include 'partials/header.php' ?>
-    <div class = "container mt-5">
-        <div class = "row">
-            <div class = "col-12">
-                asd
-            </div>
-        </div>
+  <?php include 'partials/header.php' ?>
+  <header>
+    <video onloadedmetadata="this.muted=true" autoplay loop>
+      <source src="vids/BackgroundVideo.mp4">
+    </video>
+    <div class = "container">
+      <div id = "absoluto" class = "text-center">
+        <h1 class = "display-1 mt-5">Pasteleria</h1>
+        <h3 class = "display-4">La mejor pagina de recetas</h3>
+      </div>
     </div>
-    <!-- Footer -->
-<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+  </header>
+  <div class = "container mt-3">
+  <h1 class ="display-1 text-center">Recetas del dia</h1>
+  <div class = "row mt-5">
+    <div class="col-md-4 mt-2">
+      <div class="card text-center">
+        <img src="images/platillodeldia.png" alt="platillodeldia">
+        <div class="card-body">
+          <h5 class="card-title">HOLA</h5>
+          <p class="card-text">Nose</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 mt-2">
+      <div class="card text-center">
+        <img src="images/platillodeldia.png" alt="platillodeldia">
+        <div class="card-body">
+          <h5 class="card-title">HOLA</h5>
+          <p class="card-text">Nose</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 mt-2">
+      <div class="card text-center">
+        <img src="images/platillodeldia.png" alt="platillodeldia">
+        <div class="card-body">
+          <h5 class="card-title">HOLA</h5>
+          <p class="card-text">Nose</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Footer -->
+<footer class="page-footer font-small blue pt-4 mt-5">
 
 <!-- Footer Links -->
-<div id = "footer" class="text-center text-md-left">
+<div class="container-fluid text-center text-md-left">
 
   <!-- Grid row -->
-  <div class="container row">
+  <div class="row">
 
     <!-- Grid column -->
-    <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+    <div class="col-md-6 mt-md-0 mt-3">
 
       <!-- Content -->
-      <h5 class="font-weight-bold text-uppercase mb-4">Footer Content</h5>
-      <p>Here you can use rows and columns to organize your footer content.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
-        esse
-        quasi, veritatis totam voluptas nostrum.</p>
+      <h5 class="text-uppercase">Contenido</h5>
+      <p>Hemos realizado una pagina la cual pueda acortar el tiempo de busqueda de los usuarios cuando buscar recetas se trata</p>
 
     </div>
     <!-- Grid column -->
 
-    <hr class="clearfix w-100 d-md-none">
-
     <!-- Grid column -->
-    <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+    <div class="col-md-3 mb-md-0 mb-3">
 
       <!-- Links -->
-      <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+      <h5 class="text-uppercase">COLABORADORES</h5>
 
       <ul class="list-unstyled">
         <li>
-          <p>
-            <a href="#!">PROJECTS</a>
-          </p>
+          <a href="#!">Link 1</a>
         </li>
         <li>
-          <p>
-            <a href="#!">ABOUT US</a>
-          </p>
+          <a href="#!">Link 2</a>
         </li>
         <li>
-          <p>
-            <a href="#!">BLOG</a>
-          </p>
+          <a href="#!">Link 3</a>
         </li>
         <li>
-          <p>
-            <a href="#!">AWARDS</a>
-          </p>
+          <a href="#!">Link 4</a>
         </li>
       </ul>
 
     </div>
     <!-- Grid column -->
 
-    <hr class="clearfix w-100 d-md-none">
-
     <!-- Grid column -->
-    <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+    <div class="col-md-3 mb-md-0 mb-3">
 
-      <!-- Contact details -->
-      <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+      <!-- Links -->
+      <h5 class="text-uppercase">Links</h5>
 
       <ul class="list-unstyled">
         <li>
-          <p>
-            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+          <a href="#!">Link 1</a>
         </li>
         <li>
-          <p>
-            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+          <a href="#!">Link 2</a>
         </li>
         <li>
-          <p>
-            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <a href="#!">Link 3</a>
         </li>
         <li>
-          <p>
-            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          <a href="#!">Link 4</a>
         </li>
       </ul>
-
-    </div>
-    <!-- Grid column -->
-
-    <hr class="clearfix w-100 d-md-none">
-
-    <!-- Grid column -->
-    <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
-
-      <!-- Social buttons -->
-      <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
-
-      <!-- Facebook -->
-      <a type="button" class="btn-floating btn-fb">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <!-- Twitter -->
-      <a type="button" class="btn-floating btn-tw">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <!-- Google +-->
-      <a type="button" class="btn-floating btn-gplus">
-        <i class="fab fa-google-plus-g"></i>
-      </a>
-      <!-- Dribbble -->
-      <a type="button" class="btn-floating btn-dribbble">
-        <i class="fab fa-dribbble"></i>
-      </a>
 
     </div>
     <!-- Grid column -->
@@ -152,7 +159,7 @@
 
 <!-- Copyright -->
 <div class="footer-copyright text-center py-3">© 2020 Copyright:
-  <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  <a href="https://mdbootstrap.com/"> Lucas Abdhala</a>
 </div>
 <!-- Copyright -->
 
