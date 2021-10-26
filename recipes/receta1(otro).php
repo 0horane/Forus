765464a712a443c7755c6e13621a41c2b41afb2d
Lucas-Porras <abdhalalucas@gmail.com>
2021-10-26 11:02:28 -0300

    Agregue lo de las recetas

---
+++ recipes/receta1(otro).php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<style>
+    #container-receta{
+        background-color: red;
+    }
+</style>
+<body>
+    <?php include '../partials/header.php'?>
+    <div id="container-receta" class='container'>
+        <section>
+            </a>
+            <div class="search-result-item-body">
+                <div class="row">
+                    <div class="col-sm-9">
+                        <img class="image mt-3" src="../images/chorizos_pomarola.jpg" width = "35%">
+                        <h4 class="search-result-item-heading"><a href="#">john doe</a></h4>
+                        <p class="info">New York, NY 20188</p>
+                        <p class="description">Base de chocolate
+1° paso: pre calentar el horno a 180°C. Recubrir con papel manteca la base del molde de 24 cm.
+2° paso: en un bowl batir los huevos junto con el azúcar.
+3° paso: agregar la leche. Mezclar. Verter en forma de hilo el aceite mezclando constantemente.
+4° paso: agregar en 3 veces la harina, revolver hasta disolver todos los grumos.
+5° paso: por último, incorporar el cacao en polvo previamente tamizado. Batir bien.
+6° paso: verter la mezcla en el molde y llevar a horno 180°C por 15/20 minutos. Pinchar el centro antes de retirarlo del horno. Desmoldar una vez que esté completamente frío.
+7° paso: con un poco de café y cagnac realizar un baño para el bizcochuelo.
+8° paso: recubrir la pared del molde con papel film o un acetato. Colocar la base de bizcochuelo dentro, bañarlo y reservar. Comenzar con el mousse.
+
+Mousse de chocolate
+1° paso: calentar a fuego medio la leche.
+2° paso: en una olla de tamaño medio colocar los 120 gramos de azúcar, el almidón y el café soluble. Revolver.
+3° paso: verter, a los polvos, la leche caliente en 3 veces, revolver hasta que el café se haya disuelto por completo. Llevar a fuego medio y cocinar hasta que lleve a hervor. Luego, colocar papel film a contacto a la crema y llevar a heladera hasta que esté completamente fría.
+4° paso: por otro lado, batir la crema de leche junto con los 40 gramos de azúcar. Debe quedar bien firme.
+5° paso: retirar la crema de café de la heladera, con la ayuda de la batidora mezclarla hasta que se una nuevamente. Incorporar un poco de la crema batida a la crema de café y batir.
+6° paso: luego, verter toda la crema de café dentro de la crema de leche. Mezclar con la ayuda de la batidora.
+7° paso: disolver la gelatina sin sabor con un poco de agua caliente, agregar una cucharada sopera de crema a la gelatina, unir y luego verterla dentro de la totalidad de la crema de café. Batir bien con la batidora.
+8° paso: verter la mousse dentro del molde, emparejarla y llevarla a freezer por al menos 3 horas o hasta que esté completamente congelada.
+9° paso: retirarla del freezer, desmoldar y decorar con cacao amargo en polvo. Dejar descongelar en heladera por al menos 5 horas antes de ser servida..</p>
+                    </div>
+                    <div class="col-sm-3 text-align-center">
+                        <p class="value3 mt-sm">$9, 700</p>
+                        <p class="fs-mini text-muted">PER WEEK</p><a class="btn btn-primary btn-info btn-sm" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
