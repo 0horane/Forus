8554ef72b27e18c4c37d551e468c9d59ca093735
catherine786 <cathymerida786@gmail.com>
2021-11-25 00:50:54 -0300

    ns
    Former-commit-id: b9a59afadf5b6471a0c4aa0d99c9018ccb005c85

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Recetario</title>
+    <title>Registro</title>
     <link rel="shortcut icon" href="cutlery.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -24,46 +24,47 @@
         text-align: center;
     }
     .bg{
-        background-image: url("images/imagenreglog.jpg");
         background-position: center;
         border-radius: 15px 0px 0px 15px;
     }
-    #btnLog{
-        background-color: #949492;
+    .imagen{
+        border-radius:30px;
+    }
+    .btn{
+        background-color: #09853d;
         color: white;
     }
     </style>
 </head>
 <body>
-<?php include_once "partials/header.php" ?>
     <div class="container shadow">
         <div class="row align-items-stretch">
             <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6">
+                <img class="imagen" src="images/inicio.jpg" width="560" height="590" >
             </div>
             <div class="col p-3">
                 <div class="text-end">
-                <a href="index.php"><img src="images/logo1.png" alt="logo" width="35%" href="index.php"></a>                </div>
-                <h1 id = "welcome" class="py-5 mt-3">Welcome</h1>
+                <a href="index.php"><img src="images/LogoMakr-56L0gt.png" alt="logo" width="15%" href="index.php"></a>                </div>
+                <h1 id = "welcome" class="py-5 mt-3">Bienvenido</h1>
 
             <!-- Aca vamos a empeazar a hacer todo lo que viene ser el form-->
 
                 <form class = "mt-3" action = "login.php" method = "POST"> 
                   <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nombre de usuario o mail</label>
-		    <input name="usr" type="text" class="form-control" id="usr" aria-describedby="emailHelp" placeholder="Escribí aca tu email o numbre de usuario." value="<?php echo isset($_POST["usr"]) ?  $_POST["usr"] : "";?>">
+                    <input name="usr" type="text" class="form-control" id="usr" aria-describedby="emailHelp" placeholder="Escribí aca tu email o numbre de usuario.">
                   </div>
                   <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Contraseña</label>                           
                     <input name="pwd" type="password" class="form-control" id="pwd" placeholder = "Escribí aca tu contraseña.">
                   </div>
                   <div class = "d-grid">
-                    <button id = "btnLog" type = "submit" value = "submit" class = "btn mt-2" name="loguearse">Login</button>
+                    <button type = "submit" value = "submit" class = "btn btn-outline-success" name="loguearse">Registrarme</button>
                   </div>
-				  <input name="url" type="hidden" id="url"   value="<?php echo isset($url) ?  $url : "";?>">
                 </form>
                 <div class="row text-center">
                     <div class="col-13 mt-3">
-                        <span>New here? <a href= "register.php">Register</a></span>
+                        <span>No estás registrado? <a href= "register.php">Registrarse</a></span>
                     </div>
                 </div>
                       
--- register.php
+++ register.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light+Two&family=Spartan:wght@200;300;500&display=swap" rel="stylesheet">  
     <style>
-    .container{
+  .container{
         border-radius: 15px;
         width: 100%;
         margin-top: 5%;
@@ -22,40 +22,38 @@
     #welcome{
         text-align: center;
     }
-    .bg{
-        background-image: url("images/imagenreglog.jpg");
-        background-position: center;
-        border-radius: 15px 0px 0px 15px;
+    .imagen{
+        border-radius:30px;
     }
     .btn{
-        background-color: #949492;
+        background-color: #09853d;
         color: white;
     }
     </style>
 </head>
 <body>
-    <?php include_once "partials/header.php" ?>
     <div class="container shadow">
         <div class="row align-items-stretch">
             <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6">
+            <img class="imagen" src="images/inicio.jpg" width="560" height="760" >
             </div>
             <div class="col p-3">
                 <div class="text-end">
-                    <a href="index.php"><img src="images/logo1.png" alt="logo" width="35%" href="index.php"></a>
+                    <a href="index.php"><img src="images/LogoMakr-56L0gt.png" alt="logo" width="15%" href="index.php"></a>
                 </div>
-                <h1 id = "welcome" class="py-5 mt-3">Welcome</h1>
+                <h1 id = "welcome" class="py-5 mt-3">Bienvenido</h1>
 
             <!-- Aca vamos a empeazar a hacer todo lo que viene ser el form-->
 
                 <form class = "mt-3" action = "register.php" method = "POST"> 
 				  <div class="mb-3">
                     <label for="usr" class="form-label">Nombre de Usuario</label>
-                    <input name='usr' type="text" class="form-control" id="username" aria-describedby="userHelp" placeholder="Escribí aca tu nombre de usuario." value="<?php echo isset($_POST["usr"]) ?  $_POST["usr"] : "";?>">     
+                    <input name='usr' type="text" class="form-control" id="username" aria-describedby="userHelp" placeholder="Escribí aca tu nombre de usuario." >
                     <div id="userHelp" class="form-text">Ingrese su nombre de usuario por el cual sera reconocido en nuestro sitio</div>
                   </div>
                   <div class="mb-3">
                     <label for="Email" class="form-label">Direccion de correo <!--<span style="color:gray">(opcional)</span>--></label>
-                    <input name='Email' type="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Escribí aca tu email." value="<?php echo isset($_POST["Email"]) ?  $_POST["Email"] : "";?>">
+                    <input name='Email' type="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Escribí aca tu email." >
                     <div id="emailHelp" class="form-text">Nunca compartiremos tu mail con nadie.</div>
                   </div>
                   <div class="mb-3">
@@ -68,14 +66,13 @@
                     <input name='pwdc' type="password" class="form-control" id="pwd" placeholder = "Confirma tu contraseña.">
                   </div>
                   <div class = "d-grid">
-                    <button type = "submit" value = "submit" class = "btn mt-2" name="registrarse">Registrarse</button>
+                    <button type = "submit" value = "submit" class = "btn btn-outline-success" name="registrarse">Registrarse</button>
                   </div>
                 <!-- al que le toque esta parte tiene que hacer con js que se ponhga gris el boton de sumbit hasta que el usuario y contrasenia sean iguakles-->
-				<input name="url" type="hidden" id="url"   value="<?php echo isset($url) ?  $url : "";?>">
                 </form>
                 <div class="row text-center">
                     <div class="col-13 mt-3">
-                        <span class =>¿Ya tenes una cuenta? <a href= "login.php">Loguéese</a></span>
+                        <span class =>¿Ya tenes una cuenta? <a href= "login.php">Iniciar Sesión</a></span>
                     </div>
                 </div>      
             </div>
